024717e6923ce10f6082007436d3f595a7088ea2
Dalibor Zeman <Zeman-Dalibor@users.noreply.github.com>
2019-11-09 22:34:57 +0100

    Change camera limit

--- Snooper/scripts/Camera.gd
+++ Snooper/scripts/Camera.gd
@@ -1,18 +1,21 @@
 extends Camera2D
 
+export var full_hd_x = 1920
+export var full_hd_y = 1080
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED);
+	
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		self.position += event.relative
 		
-		if self.position.x < self.limit_left:
+		if self.position.x <= self.limit_left:
 			self.position.x = self.limit_left
-		if self.position.x > self.limit_right:
+		if self.position.x >= self.limit_right:
 			self.position.x = self.limit_right
-		if self.position.y < self.limit_top:
+		if self.position.y <= self.limit_top:
 			self.position.y = self.limit_top
-		if self.position.y > self.limit_bottom:
+		if self.position.y >= self.limit_bottom:
 			self.position.y = self.limit_bottom
