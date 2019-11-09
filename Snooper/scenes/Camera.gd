11593e517fdda2c38c5049b63996f01195052a61
Aspect26 <flimmelj@gmail.com>
2019-11-09 10:21:20 +0200

    Added camera movement and random walking unit

---
+++ Snooper/scenes/Camera.gd
@@ -0,0 +1,18 @@
+extends Camera2D
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED);
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		self.position += event.relative / 3
+		
+		if self.position.x < self.limit_left:
+			self.position.x = self.limit_left
+		if self.position.x > self.limit_right:
+			self.position.x = self.limit_right
+		if self.position.y < self.limit_top:
+			self.position.y = self.limit_top
+		if self.position.y > self.limit_bottom:
+			self.position.y = self.limit_bottom
