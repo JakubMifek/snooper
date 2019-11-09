9a72cf7d89ced53c6c9f05775e350322b3fee99d
Dalibor Zeman <Zeman-Dalibor@users.noreply.github.com>
2019-11-09 18:59:06 +0100

    Add Menu

--- Snooper/scenes/Menu.gd
+++ Snooper/scenes/Menu.gd
@@ -1,15 +1,15 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
 func _input(event):
 	if event is InputEventKey:
 		if event.pressed and event.scancode == KEY_ESCAPE:
 			get_tree().quit()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_StartButton_button_down():
+	get_tree().change_scene("res://scenes/Game.tscn")
+
+func _on_CreaditsButton_button_down():
+	get_tree().change_scene("res://scenes/Credits.tscn")
+
+func _on_ExitButton_button_down():
+	get_tree().quit()
