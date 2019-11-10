654e07d6aec1df09a96fb2d983b34bf9e84c96dc
Aspect26 <flimmelj@gmail.com>
2019-11-10 18:04:26 +0200

    Added death screen

--- Snooper/scenes/ui/UI.gd
+++ Snooper/scenes/ui/UI.gd
@@ -9,4 +9,7 @@ func _ready():
 	pass # Replace with function body.
 
 func showDeath():
-	print("death")
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	var endNode = $CanvasLayer2/EndGame
+	endNode.visible=true
+	endNode.modulate.a = 1.0
