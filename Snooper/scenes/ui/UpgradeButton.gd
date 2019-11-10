00f139c0c9f4b9275e19eacd2514936ee3844efb
Michal Fibich <Michal.Fibichidis@gmail.com>
2019-11-10 11:41:38 +0100

    - upgrade button text information added

--- Snooper/scenes/ui/UpgradeButton.gd
+++ Snooper/scenes/ui/UpgradeButton.gd
@@ -1,10 +1,9 @@
-extends Sprite
+extends RichTextLabel
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position.x = texture.get_size()[0] / 2 + 4
-	position.y = OS.window_size.y - texture.get_size()[1]
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	self.rect_position = Vector2(0, OS.window_size[1]-self.rect_size[1])
